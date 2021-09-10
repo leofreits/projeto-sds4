5aeaed0aef601da4ccb7b3df93515e5aa37a471a
leofreits <leonardo.freits@gmail.com>
2021-09-10 20:40:17 -0300

    First request

--- frontend/src/components/DonutChart/index.tsx
+++ frontend/src/components/DonutChart/index.tsx
@@ -1,11 +1,40 @@
+import axios from 'axios';
 import Chart from 'react-apexcharts';
+import { SaleSum } from 'types/sale';
+import { BASE_URL } from 'utils/resquests';
+
+type ChartData = {
+    labels: string[];
+    series: number[];
+}
 
 function DonutChart() {
+//errado
+    let chartData: ChartData = {labels: [], series: []};
 
-    const mockData = {
-        series: [477138, 499928, 444867, 220426, 473088],
-        labels: ['Anakin', 'Barry Allen', 'Kal-El', 'Logan', 'Padmé']
-    }
+    
+    //errado
+
+    axios.get(`${BASE_URL}/sales/amount-by-seller`)
+    .then(response => {
+        const data = response.data as SaleSum[];
+        const myLabels = data.map(x => x.sellerName);
+        const mySeries = data.map(x => x.sum);
+
+        chartData = {labels: myLabels, series: mySeries};
+        console.log(chartData);
+
+    });
+    
+    
+    
+    
+    
+    
+    //const mockData = {
+    //    series: [477138, 499928, 444867, 220426, 473088],
+    //    labels: ['Anakin', 'Barry Allen', 'Kal-El', 'Logan', 'Padmé']
+    //}
     
     const options = {
         legend: {

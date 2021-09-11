7592b228166b26c5143c4e1b6e38284fe922c6dd
leofreits <leonardo.freits@gmail.com>
2021-09-10 21:07:23 -0300

    DonutChart integration

--- frontend/src/components/DonutChart/index.tsx
+++ frontend/src/components/DonutChart/index.tsx
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import { useEffect, useState } from 'react';
 import Chart from 'react-apexcharts';
 import { SaleSum } from 'types/sale';
 import { BASE_URL } from 'utils/resquests';
@@ -9,33 +10,35 @@ type ChartData = {
 }
 
 function DonutChart() {
-//errado
-    let chartData: ChartData = {labels: [], series: []};
 
-    
-    //errado
+    const [chartData, setChartData] = useState<ChartData>({ labels: [], series: [] });
 
-    axios.get(`${BASE_URL}/sales/amount-by-seller`)
-    .then(response => {
-        const data = response.data as SaleSum[];
-        const myLabels = data.map(x => x.sellerName);
-        const mySeries = data.map(x => x.sum);
 
-        chartData = {labels: myLabels, series: mySeries};
-        console.log(chartData);
 
-    });
-    
-    
-    
-    
-    
-    
+    useEffect(() => {
+        axios.get(`${BASE_URL}/sales/amount-by-seller`)
+            .then(response => {
+                const data = response.data as SaleSum[];
+                const myLabels = data.map(x => x.sellerName);
+                const mySeries = data.map(x => x.sum);
+
+                setChartData({ labels: myLabels, series: mySeries });
+               
+            });
+    }, []);
+
+
+
+
+
+
+
+
     //const mockData = {
     //    series: [477138, 499928, 444867, 220426, 473088],
     //    labels: ['Anakin', 'Barry Allen', 'Kal-El', 'Logan', 'Padmé']
     //}
-    
+
     const options = {
         legend: {
             show: true
@@ -43,10 +46,10 @@ function DonutChart() {
     }
     return (
         <Chart
-        options={{ ...options, labels: mockData.labels}}
-        series={mockData.series}
-        type="donut"
-        height="240"
+            options={{ ...options, labels: chartData.labels }}
+            series={chartData.series}
+            type="donut"
+            height="240"
         />
     );
 }
